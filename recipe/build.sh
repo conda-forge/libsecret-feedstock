67846642626c19663ea8762c974d4b384b7071e8
Uwe L. Korn <uwe.korn@quantco.com>
2024-12-10 13:58:05 +0100

    Set default library

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-meson setup builddir -D gtk_doc=false -D manpage=false \
+meson setup builddir -D gtk_doc=false -D manpage=false --wrap-mode=nofallback --default-library=shared \
     ${MESON_ARGS}
 
 ninja -C builddir -j${CPU_COUNT}
